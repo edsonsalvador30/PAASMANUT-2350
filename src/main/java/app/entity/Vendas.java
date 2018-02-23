f665eb84c49a9aaa379809b5a594cd049c5f4f7f
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-02-23 19:44:10 +0000

    First commit...

---
+++ src/main/java/app/entity/Vendas.java
@@ -0,0 +1,140 @@
+package app.entity;
+
+import java.io.*;
+import javax.persistence.*;
+import java.util.*;
+import javax.xml.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import cronapi.rest.security.CronappSecurity;
+
+
+/**
+ * Classe que representa a tabela VENDAS
+ * @generated
+ */
+@Entity
+@Table(name = "\"VENDAS\"")
+@XmlRootElement
+@CronappSecurity
+@JsonFilter("app.entity.Vendas")
+public class Vendas implements Serializable {
+
+  /**
+   * UID da classe, necessário na serialização
+   * @generated
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @generated
+   */
+  @Id
+  @Column(name = "id", nullable = false, insertable=true, updatable=true)
+  private java.lang.String id = UUID.randomUUID().toString().toUpperCase();
+
+  /**
+  * @generated
+  */
+  @Column(name = "nome", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.String nome;
+
+  /**
+  * @generated
+  */
+  @Column(name = "attribute_02", nullable = true, unique = false, insertable=true, updatable=true)
+  
+  private java.lang.String attribute_02;
+
+  /**
+   * Construtor
+   * @generated
+   */
+  public Vendas(){
+  }
+
+
+  /**
+   * Obtém id
+   * return id
+   * @generated
+   */
+  
+  public java.lang.String getId(){
+    return this.id;
+  }
+
+  /**
+   * Define id
+   * @param id id
+   * @generated
+   */
+  public Vendas setId(java.lang.String id){
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Obtém nome
+   * return nome
+   * @generated
+   */
+  
+  public java.lang.String getNome(){
+    return this.nome;
+  }
+
+  /**
+   * Define nome
+   * @param nome nome
+   * @generated
+   */
+  public Vendas setNome(java.lang.String nome){
+    this.nome = nome;
+    return this;
+  }
+
+  /**
+   * Obtém attribute_02
+   * return attribute_02
+   * @generated
+   */
+  
+  public java.lang.String getAttribute_02(){
+    return this.attribute_02;
+  }
+
+  /**
+   * Define attribute_02
+   * @param attribute_02 attribute_02
+   * @generated
+   */
+  public Vendas setAttribute_02(java.lang.String attribute_02){
+    this.attribute_02 = attribute_02;
+    return this;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    Vendas object = (Vendas)obj;
+    if (id != null ? !id.equals(object.id) : object.id != null) return false;
+    return true;
+  }
+
+  /**
+   * @generated
+   */
+  @Override
+  public int hashCode() {
+    int result = 1;
+    result = 31 * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+}
